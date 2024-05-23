02a25dc3e9e15bc1036a0c5038bed9559b409419
DaryaKrupenyova <dasha1458@yandex.ru>
2024-05-23 14:36:56 +0300

    9-13 work

---
+++ prac10/src/main/java/sl/Main.java
@@ -0,0 +1,22 @@
+package sl;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfig.class);
+
+        // создаем одинаковых политиков но с разными контекстами
+        Politician politician = context.getBean(Biden.class);
+        politician.doPolitic();
+
+        politician = context.getBean(Merkel.class);
+        politician.doPolitic();
+
+        politician = context.getBean(Trump.class);
+        politician.doPolitic();
+    }
+}
